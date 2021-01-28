c8f4ef7ff97bf5c6234d3d3c2a6d7afef14083a7
Raechel Walker <r6walker@ucsd.edu>
2021-01-28 10:59:53 -0800

    complete lab 4

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+
 app.get('/hello/:userName', hello.view);
 // Example route
 // app.get('/users', user.list);
